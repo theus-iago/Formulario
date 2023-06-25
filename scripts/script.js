17bc75a4caf5b33d234c5e0c8d6b01fd5b5ca1aa
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2023-06-25 15:39:03 -0300

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
-function other() {
-  location.href="cadastros.html";
+const header= document.getElementsByTagName("header")[0];
+const main= document.getElementsByTagName("main")[0];
+const form= document.getElementById("formulario-cadastro");
+const email= document.getElementById("email-cadastro");
+const nome= document.getElementById("nome-cadastro");
+const data_de_nsc= document.getElementById("data_de_nsc-cadastro");
+const senha= document.getElementById("senha-cadastro");
+
+function cadastro() {
+header.innerHTML=`email: ${email.value}<br>nome: ${nome.value}<br>data de nascimento: ${data_de_nsc.value}<br>senha: ${senha.value}`;
 }
