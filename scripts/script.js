6ded5dba2b95b2fe12cc6e86d10f3c1c0308d440
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2023-06-29 06:33:34 -0300

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 const header= document.getElementsByTagName("header")[0];
+const nav= document.getElementsByTagName('nav')[0];
+const checkbox= document.getElementById('checkbox');
+let n=0;
 const main= document.getElementsByTagName("main")[0];
 const form= document.getElementById("formulario-cadastro");
 const email= document.getElementById("email-cadastro");
@@ -9,3 +12,13 @@ const registrados= document.getElementsByClassName("registrados")[0];
 function cadastro() {
 registrados.innerHTML=`email: ${email.value}<br>nome: ${nome.value}<br>data de nascimento: ${data_de_nsc.value}<br> senha: ${senha.value}`;
 }
+function menu() {
+  if(n==0) {
+  nav.style.opacity=100;
+  n++
+  }else{
+    nav.style.opacity=0;
+    n--
+  }
+    
+  }
