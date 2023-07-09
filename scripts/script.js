38a716494eebfe9bae723b3b396a41ece713c36d
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2023-07-08 21:02:19 -0300

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+function go_to_page_cadastros() {
+  location.href="cadastros.hmtl";
+}
 function cadastro() {
 //Componentes do Cadastro:
 const email_cadastro = document.querySelector(".email-cadastro");
@@ -7,13 +10,14 @@ const c_senha = document.querySelector(".c-password");
 const btn_cadastro = document.querySelector("#btn-cadastro");
 alert(`email: ${email_cadastro.value}`);
 }
+function login() {
 //Componentes do Login:
-/*
-const email-login = document.querySelector(".email-login");
+const email_login = document.querySelector(".email-login");
 const nome = document.querySelector(".nome-login");
-const senha-login = document.querySelector(".password-cadastro");
-const btn-login = document.querySelector("#btn-login");
-*/
+const senha_login = document.querySelector(".password-cadastro");
+const btn_login = document.querySelector("#btn-login");
+alert(`Bem-Vindo ao sistema! ${email_login.value}`);
+}
 
 
 
