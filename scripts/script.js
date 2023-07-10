635e158cedf5b2e9e8d0c9ecf08f758938ea0039
Matheus Iago <120859355+theus-iago@users.noreply.github.com>
2023-07-10 05:33:34 -0300

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,16 @@ const senha_login = document.querySelector(".password-cadastro");
 const btn_login = document.querySelector("#btn-login");
 alert(`Bem-Vindo ao sistema! ${email_login.value}`);
 }
-
-
+function pass() {
+  const container = document.querySelector("#container-login");
+  const container2 = document.querySelector("#container-cadastro");
+  container.style.cssText='top:20%; left:-50%;';
+  container2.style.cssText='top:20%; left:50%; transform:translate(-50%, -20%);';
+}
+function back() {
+  const container = document.querySelector("#container-login");
+  const container2 = document.querySelector("#container-cadastro");
+  container.style.cssText='top:20%; left:50%;';
+  container2.style.cssText='top:20%; left:555%;';
+}
 
